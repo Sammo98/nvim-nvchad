93b5d3115447a597dd80aa5324fa17495b206aa1
Sam Rees <sam.rees@gmail.com>
2023-08-07 19:51:03 +0100

    buffer maps added

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -127,3 +127,9 @@ local new_cmd = vim.api.nvim_create_user_command
 new_cmd("NvChadUpdate", function()
   require "nvchad.update"()
 end, {})
+
+for i = 1, 9, 1 do
+  vim.keymap.set("n", string.format("<A-%s>", i), function()
+    vim.api.nvim_set_current_buf(vim.t.bufs[i])
+  end)
+end
